4e957f4ed9d03803dc17b761333e126f4644e5b9
Manohar Reddy A <a.manohar452@gmail.com>
2019-03-19 13:16:16 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,29 @@
-cp docker-compose.yml /app
 cd /app
+cat > docker-compose.yml <<'EOF'
+version: '2.2'
+services:
+  mongo:
+    image: mongo
+    ports: 
+      - "27017:27017"
+    volumes:
+      - /data/db:/data/db  
+    restart: always 
+  web:
+    image: manohar4524/node-mongodb:latest
+    ports: 
+      - 3000
+    environment:
+      - MONGO_HOST=mongo
+    scale: 3
+    restart: always
+  lb:
+    image: dockercloud/haproxy
+    ports:
+      - 80:80
+    links:
+      - web
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock 
+EOF
 docker-compose up -d 
